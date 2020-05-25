4723e919f0dd4c5c891286b9c3f8f3cead5d347d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-05-25 08:36:02 +0400

    Initial commit

---
+++ mongodb.ts
@@ -0,0 +1,10 @@
+// 
+import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+
+const MONGO_URL = `mongodb://localhost:27017` 
+const client = new MongoClient();
+client.connectWithUri(MONGO_URL);
+
+const db = client.database('notes');
+
+export default db;
