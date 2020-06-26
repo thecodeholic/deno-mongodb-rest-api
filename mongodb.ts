6fbcd86d04e433c41ede76abdfe01b46eff6d13a
TheCodeholic <zurasekhniashvili@gmail.com>
2020-06-26 08:17:47 +0400

    Update mongodb package version

--- mongodb.ts
+++ mongodb.ts
@@ -1,5 +1,5 @@
 // 
-import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 
 const MONGO_URL = `mongodb://localhost:27017` 
 const client = new MongoClient();
